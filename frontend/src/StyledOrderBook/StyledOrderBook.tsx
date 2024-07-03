553dd5b88147edc4360fd6ea08dcb7b61cc0cd6d
Alexandr <a.timoff3@gmail.com>
2024-07-03 17:44:28 +0300

    On SCRUM11: update style to MakeItNiceAgain `https://master--5f4988473b6fd20022f12dcc.chromatic.com/?path=/docs/orderbook--make-it-nice-again`
    also centered spread

--- frontend/src/StyledOrderBook/StyledOrderBook.tsx
+++ frontend/src/StyledOrderBook/StyledOrderBook.tsx
@@ -14,66 +14,103 @@ export default function StyledOrderBook({ book }: { book: { bids: string[][], as
       <style
         dangerouslySetInnerHTML={{
           __html: `
-            .MakeItNice {
+            .MakeItNiceAgain {
+              background-color: #151825;
+              color: rgba(255, 255, 255, 0.6);
+              display: inline-block;
               font-family: -apple-system, BlinkMacSystemFont, sans-serif;
               font-size: 13px;
               font-variant-numeric: tabular-nums;
-              display: inline-block;
-              background-color: #070F15;
-              color: rgba(255, 255, 255, 0.6);
               padding: 50px 0;
             }
 
-            .MakeItNice__side-header {
-              margin: 0 0 5px 0;
+            .MakeItNiceAgain__side-header {
               font-weight: 700;
+              margin: 0 0 5px 0;
               text-align: right;
             }
 
-            .MakeItNice__list {
+            .MakeItNiceAgain__list {
               list-style-type: none;
-              padding: 0;
               margin: 0;
+              padding: 0;
             }
 
-            .MakeItNice__list-item {
+            .MakeItNiceAgain__list-item {
+              border-top: 1px solid rgba(255, 255, 255, 0.1);
               cursor: pointer;
-              padding: 3px 50px 3px 20px;
               display: flex;
+              justify-content: flex-end;
             }
 
-            .MakeItNice__list-item:hover {
-              background: rgb(18, 29, 39);
+            .MakeItNiceAgain__list-item:before {
+              content: '';
+              flex: 1 1;
+              padding: 3px 5px;
             }
 
-            .MakeItNice__price {
-              flex: 0 0 70px;
-              color: var(--row-color);
+            .MakeItNiceAgain__side--bids .MakeItNiceAgain__list-item {
+              flex-direction: row-reverse;
+            }
+
+            .MakeItNiceAgain__side--bids .MakeItNiceAgain__list-item:last-child {
+              border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+            }
+
+            .MakeItNiceAgain__side--bids .MakeItNiceAgain__size {
               text-align: right;
+            }
+
+            .MakeItNiceAgain__list-item:hover {
+              background: #262935;
+            }
+
+            .MakeItNiceAgain__price {
+              border-left: 1px solid rgba(255, 255, 255, 0.1);
+              border-right: 1px solid rgba(255, 255, 255, 0.1);
+              color: #b7bdc1;
               display: inline-block;
-              margin-right: 15px;
+              flex: 0 0 50px;
+              margin: 0;
+              padding: 3px 5px;
+              text-align: center;
             }
 
-            .MakeItNice__size {
-              flex: 0 0 70px;
+            .MakeItNiceAgain__size {
+              flex: 1 1;
+              margin: 0;
+              padding: 3px 5px;
+              position: relative;
             }
 
-            .MakeItNice__spread {
+            .MakeItNiceAgain__size:before {
+              background-color: var(--row-color);
+              content: '';
+              height: 100%;
+              left: 0;
+              opacity: 0.08;
+              position: absolute;
+              top: 0;
+              width: 100%;
+            }
+
+            .MakeItNiceAgain__spread {
               border-width: 1px 0;
               border-style: solid;
               border-color: rgba(255, 255, 255, 0.2);
               padding: 5px 20px;
               text-align: center;
               display: flex;
+              justify-content: center;
             }
 
-            .MakeItNice__spread-header {
+            .MakeItNiceAgain__spread-header {
               margin: 0 15px 0 0;
               flex: 0 0 70px;
               text-align: right;
             }
 
-            .MakeItNice__spread-value {
+            .MakeItNiceAgain__spread-value {
               width: 28px;
               overflow: hidden;
             }
@@ -86,7 +123,7 @@ export default function StyledOrderBook({ book }: { book: { bids: string[][], as
         fullOpacity
         interpolateColor={(color) => color}
         listLength={10}
-        stylePrefix="MakeItNice"
+        stylePrefix="MakeItNiceAgain"
       />
     </>
   );
