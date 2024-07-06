6705f84d150fcbbcd59002a3ecc5c5cf41a799a4
mariohuq <15021607+mariohuq@users.noreply.github.com>
2024-07-06 22:03:53 +0300

    Fix MarketController

--- src/main/java/ru/spbstu/sce/controller/MarketController.java
+++ src/main/java/ru/spbstu/sce/controller/MarketController.java
@@ -11,7 +11,6 @@ import ru.spbstu.sce.orderbook.OrderBookResponse;
 
 import java.math.BigDecimal;
 import java.util.*;
-import java.util.stream.Collector;
 import java.util.stream.Stream;
 
 @RestController
@@ -21,7 +20,7 @@ public class MarketController {
     @Autowired
     MarketList marketList;
 
-    private Stream<OrderBookResponse.Position> formatOrderBookData(Map<BigDecimal, List<Order>> data, Comparator<BigDecimal> comparator) {
+    private List<OrderBookResponse.Position> formatOrderBookData(Map<BigDecimal, List<Order>> data, Comparator<BigDecimal> comparator, int limit) {
         return data.entrySet().stream()
                 .map(e -> new OrderBookResponse.Position(
                         e.getKey(),
@@ -30,7 +29,8 @@ public class MarketController {
                                         .stream()
                                         .map(Order::getQuantity)
                                         .reduce(0, Integer::sum))))
-                .sorted(Comparator.comparing(OrderBookResponse.Position::price, comparator));
+                .sorted(Comparator.comparing(OrderBookResponse.Position::price, comparator))
+                .limit(limit).toList();
     }
 
     @GetMapping("/orderbook")
@@ -42,26 +42,9 @@ public class MarketController {
             throw new IllegalArgumentException("symbol %s does not exists".formatted(symbol));
         }
         return new OrderBookResponse(symbol,
-                formatOrderBookData(marketList.getBidMap(symbol), Comparator.reverseOrder()).collect(lastNCollector(limit)),
-                formatOrderBookData(marketList.getOfferMap(symbol), Comparator.naturalOrder()).limit(limit).toList(),
+                formatOrderBookData(marketList.getBidMap(symbol), Comparator.reverseOrder(), limit),
+                formatOrderBookData(marketList.getOfferMap(symbol), Comparator.naturalOrder(), limit),
                 System.currentTimeMillis());
     }
 
-    // collects stream into list with n last elements of it
-    private <T> Collector<T, Deque<T>, List<T>> lastNCollector(int maxSize) {
-        return Collector.of(
-                () -> new ArrayDeque<>(maxSize),
-                (list, e) -> {
-                    if (list.size() >= maxSize) {
-                        list.removeFirst();
-                    }
-                    list.add(e);
-                },
-                (leftList, rightList) -> {
-                    while (rightList.size() < maxSize && !leftList.isEmpty()) {
-                        rightList.addFirst(leftList.removeLast());
-                    }
-                    return rightList;
-                }, ArrayList::new);
-    }
 }
