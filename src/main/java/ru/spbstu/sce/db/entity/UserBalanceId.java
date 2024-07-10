84c08ca3b2104b5ba9dcb5ce7d9e37956c504c82
mariohuq <15021607+mariohuq@users.noreply.github.com>
2024-07-10 14:00:55 +0300

    remove unnecessary aannotations

--- src/main/java/ru/spbstu/sce/db/entity/UserBalanceId.java
+++ src/main/java/ru/spbstu/sce/db/entity/UserBalanceId.java
@@ -1,6 +1,5 @@
 package ru.spbstu.sce.db.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
 
@@ -10,9 +9,9 @@ import java.util.Arrays;
 @Getter
 @Embeddable
 public class UserBalanceId implements Serializable {
-    @Column(name = "user_id")
+
     private long userId;
-    @Column(name = "coin_id")
+
     private long coinId;
 
     public UserBalanceId() {
