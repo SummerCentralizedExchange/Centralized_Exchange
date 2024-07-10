01dfdd8224e95483303ef4a1eb4bbd200efc7fb5
Alexandr <a.timoff3@gmail.com>
2024-07-10 20:03:45 +0300

    On SCRUM24: using jackson to impl test.

--- src/test/java/ru/spbstu/sce/orderbook/OrderControllerTest.java
+++ src/test/java/ru/spbstu/sce/orderbook/OrderControllerTest.java
@@ -1,5 +1,6 @@
 package ru.spbstu.sce.orderbook;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -36,9 +37,19 @@ public class OrderControllerTest {
 
     @Test
     public void testCreateOrder() throws Exception {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setPrice(new BigDecimal(10000));
+        orderItem.setQuantity(new BigDecimal(1));
+        orderItem.setSymbol("BTCUSDT");
+        orderItem.setType("Limit");
+        orderItem.setSide("Buy");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonData = objectMapper.writeValueAsString(orderItem);
+
         mockMvc.perform(post("/order/create")
                         .contentType("application/json")
-                        .content("{\"coinName\":\"BTCUSDT\",\"price\":10000,\"quantity\":1,\"type\":\"Limit\",\"transactionType\":\"Buy\"}"))
+                        .content(jsonData))
                 .andExpect(status().isOk());
 
         verify(market, times(1)).bidAdd(any(OrderItem.class));
